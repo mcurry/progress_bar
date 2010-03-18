391a7a0e99cca0796e870df8b6b76a7bbbede533
predominant <graham@grahamweldon.com>
2010-03-19 00:07:40 +1100

    Added doc blocks and minor code tidy.

--- vendors/shells/tasks/progress_bar.php
+++ vendors/shells/tasks/progress_bar.php
@@ -10,26 +10,86 @@
  *
  */
 
+/**
+ * Progress Bar Task
+ *
+ * @package progress_bar
+ * @subpackage progrss_bar.vendors.shells.tasks
+ */
 class ProgressBarTask extends Shell {
+
+/**
+ * Maximum value on the bar
+ *
+ * @var int
+ * @access public
+ */
 	var $total = 100;
+
+/**
+ * Size
+ *
+ * @var int
+ * @access public
+ */
 	var $size = 25;
+
+/**
+ * Amount Completed
+ *
+ * @var int
+ * @access public
+ */
 	var $done = 0;
+
+/**
+ * Start Time
+ *
+ * @var mixed
+ * @access public
+ */
 	var $startTime = null;
 
+/**
+ * Execute the task
+ *
+ * @return void
+ * @access public
+ */
 	function execute() {	}
-	
+
+/**
+ * Start
+ *
+ * @param string $total Total value of the progress bar
+ * @return void
+ * @access public
+ */
 	function start($total) {
 		$this->total = $total;
 		$this->done = 0;
 		$this->startTime = time();
 	}
 
+/**
+ * Increment the progress
+ *
+ * @return void
+ * @access public
+ */
 	function next() {
-		$this->done ++;
+		$this->done++;
 		$this->set();
 	}
 
-	function set($done=null) {
+/**
+ * Set the values and output
+ *
+ * @param string $done Amount completed
+ * @return void
+ * @access public
+ */
+	function set($done = null) {
 		if ($done) {
 			$this->done = min($done, $this->total);
 		}
@@ -37,17 +97,24 @@ class ProgressBarTask extends Shell {
 		$perc = round($this->done / $this->total, 3);
 		$doneSize = floor($perc * $this->size);
 
-		$output = sprintf("\r[%s>%s] %.01f%% %d/%d %s remaining%s",
-											str_repeat("-", $doneSize),
-											str_repeat(" ", $this->size - $doneSize),
-											$perc * 100,
-											$this->done, $this->total,
-											$this->niceRemaining(),
-											str_repeat(' ', 10));
-		echo $output;
+		echo sprintf(
+			"\r[%s>%s] %.01f%% %d/%d %s %s%s",
+			str_repeat("-", $doneSize),
+			str_repeat(" ", $this->size - $doneSize),
+			$perc * 100,
+			$this->done, $this->total,
+			$this->niceRemaining(),
+			__('remaining', true),
+			str_repeat(' ', 10));
 		flush();
 	}
-	
+
+/**
+ * Calculate remaining time in a nice format
+ *
+ * @return void
+ * @access public
+ */
 	function niceRemaining() {
 		$now = time();
 		if($now == $this->startTime) {
